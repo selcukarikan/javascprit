6cf6e6ec26c93959519ce2ed7ce6dba2d40388e9
Selcuk Arikan <selcukarikan1301@gmail.com>
2021-10-01 21:11:52 +0200

    nesneler

--- ders5_diziler/ornek3_iterasyon/iterasyon.js
+++ ders5_diziler/ornek3_iterasyon/iterasyon.js
@@ -4,13 +4,30 @@
 
 const koordinatlar = [120, 100, -100, 220, 330, -50];
 
-let pozitif=0;
-let negatif=0;
+let pozitif = 0;
+let negatif = 0;
 
-for(let i =0; i<koordinatlar.length; i++){
-
-
-
- koordinatlar[i] < 0 ? negatif=negatif+koordinatlar[i] : pozitif = pozitif+ koordinatlar[i];
+for (let i = 0; i < koordinatlar.length; i++) {
+  koordinatlar[i] < 0
+    ? (negatif = negatif + koordinatlar[i])
+    : (pozitif = pozitif + koordinatlar[i]);
 }
 console.log(`negatifler toplamı: ${negatif} pozifler toplamı: ${pozitif}`);
+
+//todo FOR IN DONGUSU
+
+const hayvanlar=["aslan","fil","kartal","kanarya","civciv","kartal"];
+const ara=prompt("sayisini merak ettigin hayvanin adini yaziniz");
+const bulDondur=function(ara){
+
+let hayvanSayisi=0;
+for(let i in hayvanlar)
+{
+    if(hayvanlar[i]==ara)
+    {hayvanSayisi++;}
+
+}
+hayvanSayisi==0?alert("aradigin hayvan bulunamadi"):
+alert(àradiginiz${ara}hayvanindan ${hayvanSayisi} tane bulunmaktadir`);
+}
+bulDondur(ara);
---
+++ ornek-6Nesneler/nesneler.js
@@ -0,0 +1,96 @@
+// Nesneler
+
+//diziler sirali bellek bölgeleridir. dolayisiyla dizilere erisim index ile olur. eger diziler karisik ise (nesne)
+//indexle olur. Eger diziler karisiksa (nesne) indexle erisim guclugu ortaya cikar bu yuzden =>
+
+//! Object (nesne)
+
+const insan = {
+  ad: "selcuk",
+  soyad: "arikan",
+  yas: 36,
+  meslek: "QA Test Automation Enginner",
+  diller: ["java", "js", "sql", "html"]
+};
+console.log(insan);
+
+//1- .dot notasyonu
+console.log(insan.ad);
+console.log(insan.diller);
+
+//2-koseli parantez icinde gosterimi
+console.log(insan["soyad"]);
+console.log(insan["diller"]);
+
+console.log(`Adim:${insan.ad} ve soyadim:${insan.soyad}`);
+
+// const bilgi = prompt(
+// "insan nesnesinden hangisini gormek istersin -ad,soyad,yas..-"
+// );
+// console.log(insan[bilgi]);
+
+//objeye.key.value lar ekleme
+
+insan.konum = "Turkiye";
+insan.email = "selcukarikan1301@gmail.com";
+insan["dogumTarihi"] = 1986;
+
+console.log(insan);
+
+const kisi = {
+  ad: "belma",
+  soyad: "arikan",
+  dogumTarihi: 2016,
+  meslek: "prenses",
+  tool: "sempatiklik",
+  ehliyet: false,
+  yasHesapla: function () {
+    return new Date().getFullYear() - this.dogumTarihi;
+  },
+  ozet: function () {
+    return ` ${this.ad} ${this.soyad} ${this.yasHesapla()} yasındadır`;
+  }
+};
+console.log(kisi);
+
+console.log(kisi.yasHesapla());
+
+console.log(kisi.ozet());
+
+// nesne (object)iterasyon ornekleri
+const people = [
+  {
+    ad: "ali",
+    soyad: "arkan",
+    meslek: "ogrenci",
+    yas: 8
+  },
+  {
+    ad: "ahmet",
+    soyad: "arkan",
+    meslek: "sekercocuk",
+    yas: 1
+  },
+  {
+    ad: "belma",
+    soyad: "arkan",
+    meslek: "prenses",
+    yas: 5
+  },
+  {
+    ad: "leyla",
+    soyad: "arkan",
+    meslek: "evhanimi",
+    yas: 33
+  }
+];
+
+console.log(people);
+
+// ornek 1: people dizisindeki kisilerin mesleklerini konsolda yazdiralim
+
+people.forEach((x) => console.log(x.meslek));
+
+//ornek 2: people dizisindeki tum kisilerin yaslarini 1 artirarak yazdiriniz
+
+people.map((x) => x.yas + 1).forEach((x) => console.log(x));
